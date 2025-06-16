e42430d60fce63c2823c0bb9ac4a1f3b321f6d47
ShubhamPatilSp <xshuubhampatil@gmail.com>
2025-06-16 12:53:43 +0530

    add

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,10 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   async rewrites() {
+    const backendUrl = process.env.NEXT_PUBLIC_BACKEND_URL || 'http://localhost:8000';
     return [
       {
         source: "/api/v1/:path*", // More specific path to avoid conflict with Auth0 routes
-        destination: "http://localhost:8000/api/:path*", // Proxy to Backend
+        destination: `${backendUrl}/api/:path*`, // Proxy to Backend
       },
     ];
   },
