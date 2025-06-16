a706af68fb1de975038628f9cb60dc7e0d1b477c
ShubhamPatilSp <xshuubhampatil@gmail.com>
2025-06-16 13:42:20 +0530

    fix: update routing and environment variables

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -8,13 +8,16 @@ const nextConfig = {
         destination: `${backendUrl}/api/v1/$1`, // Proxy to Backend
       },
       {
-        source: "/api/auth/callback",
-        destination: `${backendUrl}/api/auth/callback`
+        source: "/api/auth/(.*)", // Handle all Auth0 routes
+        destination: `${backendUrl}/api/auth/$1`
       }
     ];
   },
   env: {
-    NEXT_PUBLIC_AUTH0_BASE_URL: process.env.NEXT_PUBLIC_AUTH0_BASE_URL || 'https://status-track-alpha.vercel.app'
+    NEXT_PUBLIC_AUTH0_BASE_URL: process.env.NEXT_PUBLIC_AUTH0_BASE_URL || 'https://status-track-alpha.vercel.app',
+    NEXT_PUBLIC_AUTH0_ISSUER_BASE_URL: process.env.NEXT_PUBLIC_AUTH0_ISSUER_BASE_URL,
+    NEXT_PUBLIC_AUTH0_CLIENT_ID: process.env.NEXT_PUBLIC_AUTH0_CLIENT_ID,
+    NEXT_PUBLIC_AUTH0_AUDIENCE: process.env.NEXT_PUBLIC_AUTH0_AUDIENCE
   }
 };
 
