a3c3811e6f4d40cf32f0290e8fca25bea5c84ae2
ShubhamPatilSp <xshuubhampatil@gmail.com>
2025-06-16 13:30:26 +0530

    add

--- frontend/src/app/api/auth/config.ts
+++ frontend/src/app/api/auth/config.ts
@@ -1,10 +1,12 @@
-import { init } from '@auth0/nextjs-auth0';
+import { handleAuth } from '@auth0/nextjs-auth0';
 
-init({
+export default handleAuth();
+
+export const config = {
   baseURL: process.env.NEXT_PUBLIC_AUTH0_BASE_URL || 'http://localhost:3000',
   issuerBaseURL: process.env.NEXT_PUBLIC_AUTH0_ISSUER_BASE_URL,
-  clientID: process.env.AUTH0_CLIENT_ID,
+  clientId: process.env.AUTH0_CLIENT_ID,
   clientSecret: process.env.AUTH0_CLIENT_SECRET,
   audience: process.env.NEXT_PUBLIC_AUTH0_AUDIENCE,
   secret: process.env.AUTH0_SECRET,
-});
+};
